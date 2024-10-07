f2fbb964d40b90536f2ce72ebd3e93ae67459bbe
MinhyeokChoco <smile_forever@nate.com>
2024-10-07 21:35:30 +0900

    [feat] : swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors({
     origin: [process.env.ENV === "development" ? "http://locahost:3000" : "http://aws주소"],
     methods: ["POST", "GET", "PUT", "DELETE"],
@@ -12,19 +13,24 @@ async function bootstrap() {
   })
 
   // Swagger 설정
-  const config = new DocumentBuilder().setTitle("ergani API").setDescription("ergani api 문서입니다.").addTag("User").addTag("Post").addTag("Comment").addTag("Reply").addTag("PostLikes").addTag("CommentLikes").addTag("ReplyLikes").addBearerAuth(
-    {
+  const config = new DocumentBuilder()
+    .setTitle("UPHiLL API")
+    .setDescription("UPHiLL api 문서입니다.")
+    .addTag("카카오")
+    .addTag("User")
+    .addBearerAuth({
       type: 'http',
       scheme: 'bearer',
       name: 'JWT',
       in: 'header',
     },
-    'access-token',
-  ).build();
+      'access-token',
+    ).build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
